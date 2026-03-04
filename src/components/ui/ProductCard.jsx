ae2214e91174ebf0bc033d8c7f8cda9473a30da2
ezequiel <alcayaga147147@gmail.com>
2026-03-04 13:40:31 -0300

    fix: add rounded styling to eye button in ProductCard component

--- src/components/ui/ProductCard.jsx
+++ src/components/ui/ProductCard.jsx
@@ -4,7 +4,7 @@ export default function ProductCard({ imageUrl, name, price }) {
     <div>
       <div className="relative  bg-gray-100 p-6">
        {/* <div className="absolute top-2 left-2 h-8 w-8 flex items-center justify-center"> */}
-            <button className="absolute top-2 left-2 z-10 h-8 w-8 p-0 bg-white  flex items-center justify-center"><img src={eyeIcon} alt="Ver producto" className="absolute h-6 w-6 block " /></button>
+            <button className="absolute top-2 left-2 z-10 h-8 w-8 p-0 bg-white rounded-full flex items-center justify-center"><img src={eyeIcon} alt="Ver producto" className="absolute h-6 w-6 block " /></button>
     
        {/* </div> */}
         <img src={imageUrl} alt={name} className="w-full object-cover" />
